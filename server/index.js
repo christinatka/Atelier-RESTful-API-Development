693f5e897a034899a70f61a5a70c36c7d8523e6c
Christina Tkatchenko <christinatka@gmail.com>
2021-02-09 13:23:06 -0500

    update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = require('./routes.js');
 const cors = require('cors');
 
-//require('../ETL/streams/run-streams.js');
+require('../ETL/streams/run-streams.js');
 
 const port = process.env.PORT || 3333;
 const app = express();
